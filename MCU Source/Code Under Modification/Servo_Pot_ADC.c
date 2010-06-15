51cf57237fa4620fd999682574db8dafa9d664b6
PaladinoJ <PaladinoJ@gmail.com>
2010-06-15 10:59:14 -0500

    Added some files that will be the framework for the potentiometer code for reading true steering angle.
    Signed-off-by: PaladinoJ <PaladinoJ@gmail.com>

---
+++ MCU Source/Code Under Modification/Servo_Pot_ADC.c
@@ -0,0 +1,16 @@
+// filename ******** Servo_Pot_ADC.h **************
+// A Servo Potentiometer driver for the Proteus Robot
+// For use with a Potentiometer that corresponds to the steering servo
+// Modified by Justin Paladino {PaladinoJ@gmail.com}
+/*
+ ** Pin connections **
+    
+
+*/
+
+#include <mc9s12dp512.h>     /* derivative information */
+#include "adc.h"
+#include "Servo_Pot_ADC.h"
+
+
+
---
+++ MCU Source/Code Under Modification/Servo_Pot_ADC.h
@@ -0,0 +1,10 @@
+// filename ******** Servo_Pot_ADC.h **************
+// A Servo Potentiometer driver for the Proteus Robot
+// For use with a Potentiometer that corresponds to the steering servo
+// Modified by Justin Paladino {PaladinoJ@gmail.com}
+/*
+ ** Pin connections **
+    
+
+*/
+
