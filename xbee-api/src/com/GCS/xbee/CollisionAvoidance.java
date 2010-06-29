6afc8289b53f087b2201c6d727e164d3790f483e
Varun Sampath <vsampath@seas.upenn.edu>
2010-06-29 16:10:01 -0500

    Added collision avoidance code

--- xbee-api/src/com/GCS/xbee/CollisionAvoidance.java
+++ xbee-api/src/com/GCS/xbee/CollisionAvoidance.java
@@ -48,8 +48,6 @@ public class CollisionAvoidance {
 	
 	// Assume data has 3 fields: latitude, longitude, and altitude
 	private void transmit(XBeeAddress64 addr, int[] waypoint) {
-		if (waypoint.length != 3) throw new IllegalArgumentException();
-		
 		int payload[] = new int[16];	//4 int32's
 		
 		// ArduPilot asks for these multipliers
@@ -89,9 +87,17 @@ public class CollisionAvoidance {
 		waypoint[0] /= 10; waypoint[1] /= 10; waypoint[2] /= 100;
 		log.info("sent " + Arrays.toString(waypoint) + " to Plane " + dataMap.get(addr).num);
 	}
+
+	private class Coordinate {
+		double x;
+		double y;
+		double z;
+	}
+
 	
 	// thread that runs collision avoidance algorithm
 	private class Avoid implements Runnable {
+
 		@Override
 		public void run() {
 			synchronized (dataMap) {
@@ -102,11 +108,75 @@ public class CollisionAvoidance {
 					
 					// Do algorithm calculations in here after getting told from hash map about new data
 					XBeeAddress64 addr = latest;
+					Coordinate sumRepulsive = new Coordinate();
+					sumRepulsive.x = 0; sumRepulsive.y = 0; sumRepulsive.z = 0;
+
+					for (PlaneData airplane : dataMap.values()) {
+						if (airplane != dataMap.get(latest)) {
+							Coordinate repulse = new Coordinate();
+							forceRepulsive(dataMap.get(latest), airplane, repulse);
+							log.debug("The repulsive force vector is: <" + repulse.x + "," + repulse.y + ">");
+							sumRepulsive.x += repulsive.x;
+							sumRepulsive.y += repulsive.y;
+						}
+					}
+
+					Coordinate attract = new Coordinate();
+					forceAttractive(latest, attract);
+
+					Coordinate resultant = new Coordinate();
+					resultant.x = sumRepulsive.x + attract.x;
+					resultant.y = sumRepulsive.y + attract.y;
+
+					log.debug("The resultant force vector is: <" + resultant.x + "," + resultant.y + ">");
+
+					Coordinate newWP = new Coordinate();
+					newWP.x = resultant.x + dataMap.get(latest).currLat;
+					newWP.y = resultant.y + dataMap.get(latest).currLng;
+					newWP.z = dataMap.get(latest).currAlt;
+
 					int[] waypoint = {32594727,-85497500,250};		// south intramural field, HIGH alt
 //					transmit(addr, waypoint);
 				}
 			}
 		}
+
+		private double distance2D(double x1, double y1, double x0, double y0) {
+			return Math.sqrt(Math.pow(x1-x0,2) + Math.pow(y1-y0,2));
+		}
+
+		private void forceRepulsive(PlaneData airplane, PlaneData active, Coordinate repulse) {
+			double distance;
+			double x, y;
+			int[] active = new int[3];
+			final double Fcr = 10;
+
+			distance = distance2D(active.currLat, active.currLng, airplane.currLat, airplane.currLng);
+
+			x = ((airplane.currLat - active.currLat) / distance);
+			y = ((airplane.currLng - active.currLng) / distance);
+
+			log.debug("The repulsive force unit vector is <" + x + "," + y + ">");
+			repulse.x = (Fcr / Math.pow(distance, 2)) * ((airplane.x - active.x) / distance);
+			repulse.y = (Fcr / Math.pow(distance, 2)) * ((airplane.y - active.y) / distance);
+		}
+
+		private void forceAttractive(PlaneData airplane, Coordinate attract) {
+			double distance;
+			double x, y, x2, y2;
+			final double Fct = 5;
+
+			distance = distance2D(airplane.nextLat, airplane.nextLng, airplane.currLat, airplane.currLng);
+
+			x = ((airplane.nextLat - airplane.currLat) / distance);
+			y = ((airplane.nextLng - airplane.currLng) / distance);
+
+			log.debug("The attractive force unit vector is: <" + x + "," + y + ">");
+
+			attract.x = Fct*(airplane.nextLat - airplane.currLat) / distance;
+			attract.y = Fct*(airplane.nextLng - airplane.currLng) / distance;
+		}
+
 		
 	}
 
