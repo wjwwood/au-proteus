9ec53167317b71269a7fc40cfd85e1f1faa050f5
Justin Paladino <PaladinoJ@gmail.com>
2010-07-06 10:24:24 -0500

    This is just a changed comment.
    Signed-off-by: Justin Paladino <PaladinoJ@gmail.com>

--- MCU Source/Code Under Modification/Command.h
+++ MCU Source/Code Under Modification/Command.h
@@ -52,7 +52,7 @@ enum{
 };
 
 #define PROTEUS_ODOMETRY_PACKET_SIZE      5
-#define PROTEUS_IR_PACKET_SIZE           12  //12 for standard 6 IR ... 28 if extra IR needed/used
+#define PROTEUS_IR_PACKET_SIZE           12  //12 for standard 6 IR ... up to 28 if extra IR needed/used
 #define PROTEUS_SONAR_PACKET_SIZE        12
 #define PROTEUS_COMPASS_PACKET_SIZE       2
 #define PROTEUS_SERVO_PACKET_SIZE         2
